b86528c38673d937d5424b64f7c27d8b8e75b47f
zerosenn52gata <a.sasaki.sys19@morijyobi.ac.jp>
2020-06-26 12:26:20 +0900

    a

--- index.php
+++ index.php
@@ -69,7 +69,52 @@ try {
                 </table>
             </div>
             <div class="tab-pane" id="insert">
-                <p>sample tab 2</p>
+            <form class="mt-3" action="./insert.php" method="POST">
+
+<div class="form-group row">
+
+    <label for="id" class="col-sm-2 col-form-label">ID</label>
+
+    <div class="col-sm-10">
+
+        <input type="text" class="form-control" id="id" name="id">
+
+    </div>
+
+</div>
+
+<div class="form-group row">
+
+    <label for="name" class="col-sm-2 col-form-label">Your Name</label>
+
+    <div class="col-sm-10">
+
+        <input type="text" class="form-control" id="name" name="name">
+
+    </div>
+
+</div>
+
+<div class="form-group row">
+
+    <label for="age" class="col-sm-2 col-form-label">Age</label>
+
+    <div class="col-sm-10">
+
+        <input type="text" class="form-control" id="age" name="age">
+
+    </div>
+
+</div>
+
+<div class="form-group">
+
+    <button type="submit" class="btn btn-primary btn-block">Insert</button>
+
+</div>
+
+</form>
+
             </div>
             <div class="tab-pane" id="update">
                 <p>sample tab 3</p>
