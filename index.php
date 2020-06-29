79f9ef1b3f05a01d7ae30a7c80cf232a9ccdba13
zerosenn52gata <a.sasaki.sys19@morijyobi.ac.jp>
2020-06-29 11:27:08 +0900

    a

--- index.php
+++ index.php
@@ -216,7 +216,51 @@ try {
 
 			<div class="tab-pane" id="update">
 
-				<p>sample tab 3</p>
+			<form class="mt-3" action="./update.php" method="POST">
+
+<div class="form-group row">
+
+	<label for="id" class="col-sm-2 col-form-label">ID</label>
+
+	<div class="col-sm-10">
+
+		<input type="text" class="form-control" id="id" name="id">
+
+	</div>
+
+</div>
+
+<div class="form-group row">
+
+	<label for="name" class="col-sm-2 col-form-label">Your Name</label>
+
+	<div class="col-sm-10">
+
+		<input type="text" class="form-control" id="name" name="name">
+
+	</div>
+
+</div>
+
+<div class="form-group row">
+
+	<label for="age" class="col-sm-2 col-form-label">Age</label>
+
+	<div class="col-sm-10">
+
+		<input type="text" class="form-control" id="age" name="age">
+
+	</div>
+
+</div>
+
+<div class="form-group">
+
+	<button type="submit" class="btn btn-primary btn-block">Insert</button>
+
+</div>
+
+</form>>
 
 			</div>
 
---
+++ update.php
@@ -0,0 +1,47 @@
+<?php
+
+$dsn = 'mysql:dbname=test_db;host=localhost;';
+
+$user = 'sasaki';
+
+$password = 'zerosenn52gata';
+
+try {
+
+    $dbh = new PDO($dsn, $user, $password);
+
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+    $id = $_POST['id'];
+
+    $name = $_POST['name'];
+
+    $age = $_POST['age'];
+
+
+
+    $sql = "update user set name=:name, age=:age where id = :id";
+
+    $stmt = $dbh->prepare($sql);
+
+    $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
+
+    $stmt->execute($prams);
+
+
+
+    header('Location: index.php?fg=1');
+
+    
+
+} catch (PDOException $e) {
+
+	header('Location: index.php?fg=2?err='.$e->getMessage());
+
+	exit();
+
+}
+
+?>
